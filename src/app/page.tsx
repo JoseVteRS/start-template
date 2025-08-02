38439152a53b142cd1a79a2e4f8df36ad1cd844d
Jose <jvrs.90@gmail.com>
2025-08-02 06:10:32 +0200

    shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,20 @@ export default function Home() {
   prefetch(trpc.posts.get.queryOptions());
 
   return (
-    <div>
-      <main>
-        <h1>Start Template</h1>
-        <p>
-          With TailwindCSS, Shadcn UI, Drizzle ORM, BetterAuth, React Hook Form,
-          React Query, Zod, SuperJSON and TRPC
+    <div className="min-h-screen bg-gradient-to-b from-blue-50 to-white">
+      <main className="container mx-auto px-4 py-16 flex flex-col items-center">
+        <h1 className="text-4xl md:text-5xl font-bold text-center bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent mb-6">
+          Start Template
+        </h1>
+        <p className="text-lg text-gray-600 text-center max-w-2xl mb-12">
+          Con TailwindCSS, Shadcn UI, Drizzle ORM, BetterAuth, React Hook Form,
+          React Query, Zod, SuperJSON y TRPC
         </p>
-        <HydrateClient>
-          <Posts />
-        </HydrateClient>
+        <div className="w-full max-w-5xl">
+          <HydrateClient>
+            <Posts />
+          </HydrateClient>
+        </div>
       </main>
     </div>
   );
--- src/module/posts/posts.tsx
+++ src/module/posts/posts.tsx
@@ -1,19 +1,27 @@
 "use client"
 
+import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
 import { useQuery } from '@tanstack/react-query';
+import Link from "next/link";
 
 export const Posts =()=> {
     const trpc = useTRPC()
     const data = useQuery(trpc.posts.get.queryOptions())
 
     return (
-        <div className="flex flex-col gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {data.data?.map((post)=> (
-                <div key={post.id} className="flex flex-col gap-2 bg-zinc-800 p-4 rounded-md">
+                <div key={post.id} className="flex flex-col gap-2 bg-zinc-100 p-4 rounded-md">
                     <h1 className="text-2xl font-bold">{post.title}</h1>
                     <p className="text-sm text-gray-500">{post.content}</p>
+                    <Button asChild>
+                        <Link href={`/posts/${post.id}`}>
+                            View
+                        </Link>
+                    </Button>
                 </div>
+
             ))}
         </div>
     )
